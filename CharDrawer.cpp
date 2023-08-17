d5ff853df233ba1f5c49ad7613bbd3ea380c483d
g jardine <gail.k.jardine@outlook.com>
2023-08-17 14:23:16 -0400

    prototyping: filled in much of the drawing and cursor moving code. Need to check for off-by-one errors.
    prototype: basic functions written, preparing to revise print() and prepareCursor()

--- CharDrawer.cpp
+++ CharDrawer.cpp
@@ -1,10 +1,32 @@
 #include "CharDrawer.h"
-using std::cin; using std::cout; using std::endl; using std::ios;
+using std::cin; using std::cout; using std::endl; using std::ios; using std::ios_base;
 using std::string;
 
-
-int CharDrawer::nLastCol() const;
-int CharDrawer::nLastRow() const;
+//Count number of characters (not \n or \0) in given row.
+int CharDrawer::lengthRow(int nRow) {
+  string str;
+  fs.seekg(0, fs.beg);
+  for (int i = 0; i <= nRow; ++i)
+    if(!getline(fs, str)) //failed to read! assume row does not exist
+      return 0;
+  return str.size();
+}
+int CharDrawer::lengthLongestRow() {
+  int nRowLength = 0;
+  for (int i = 0; i <= nRowEnd(); ++i)
+    nRowLength = maxsimp(nRowLength, lengthRow(i));
+  return nRowLength;
+}
+//returns 1 plus the index of the last row,
+//that is, returns the number of rows.
+int CharDrawer::nRowEnd() {
+  string str;
+  int i = 0;
+  fs.seekg(0, fs.beg);
+  while (getline(fs, str))
+    i++;
+  return i;
+}
 /* Format of printing:
 ideally, it could be like this:
   12345.789111
@@ -34,43 +56,116 @@ honestly, this is actually more effort than I want to put into it....
   bDrawingMode determines whether (false) we print the image as it is (still maintaining the row and column),
   or (true) we use '.' to represent the cursor on the image.
 */
-void CharDrawer::print(bool bDrawingMode = false) const { //prints. we call this after every change.
+void CharDrawer::print(bool bDrawingMode = false) {
+  system("cls"); //clear screen, for the fun of it.
+  int nEnd = nRowEnd();
+  fs.seekg(0, fs.beg);
   //print top row with a '.' so we know where we are
-  iostream is
+  if(bDrawingMode) {
+    pad(cout, 0, nCursorCol);
+    cout << ".\n";
+  }
   //print each row.
-  int nEnd = nLastRow();
   for(int i = 0; i < nEnd; ++i) {
     //first, print the '.' that shows where we are
     //now print the row.
     if(bDrawingMode && i == nCursorRow) { //also draw '.' as the cursor
+      cout << '.';
       int j = 0;
       char c;
-      //by assumption, nCursorCall is at most 1 past the last drawn pt.
+      //by assumption, nCursorCol is at a valid point, or else 1 past the end of the line
       //otherwise, going right will automatically print a ' ' to the file,... or maybe be prohibited.
-      while(is.get(c) || j == nCursorCall) {
+      while(fs.get(c) || j == nCursorCol) {
         if (j++ == nCursorCol) cout << '.';
         else                   cout << c;
         if (c == '\n' && nCursorCol > j)
           break; //come to the end
       }
-      cout << "" << endl;
+      cout << '|' << endl;
     }
     else {
+      if(bDrawingMode) cout << ' '; 
       string strLine;
-      std::getline(is, strLine);
-      cout << strLine << endl;
+      std::getline(fs, strLine);
+      cout << strLine << '|' << endl;
     }
   }
 }
-
-
-void CharDrawer::draw() { //this one is a world to itself.
+/* this function puts the cursor in position using prepareCursor, then writes the character. */
+void CharDrawer::writeValAtCursor(char c) {
+  prepareCursor();
+  fs << c;
+}
+void CharDrawer::draw() {
   string str;
-  while (cin.get(str)) {
-
-    cout << "To end \'draw\' mode and return to the main menu, press Esc." << endl;
-    print(true);
+  keyboardActionKey::e key;
+  //assumption: at the start of the loop, the cursor is at a valid position.
+  //we have to ensure this ourselves. it is a fragile position.
+  prepareCursor();
+  while (true) {
+    print(true); //so far, print() clears the screen first.
+    cout << "To exit \'draw\' mode and return to the main menu, press Esc." << endl;
+    
+    char cOutput = getKey(key);
+    switch(key) {
+      case keyboardActionKey::character:
+        fs << cOutput; //writeValAtCursor(output); //possibly safer, but....
+        //no break -- because we also go right after making a character
+      case keyboardActionKey::arrowRight;
+        nCursorCol++;
+        break;
+      case keyboardActionKey::arrowLeft;
+        nCursorCol--;
+        break;
+      case keyboardActionKey::arrowUp;
+        nCursorRow--;
+        break;
+      case keyboardActionKey::arrowDown;
+        nCursorRow++;
+        break;
+      case keyboardActionKey::esc;
+        navigate();
+        break;
+      default:
+    }
+    prepareCursor();
   }
+}
+
+/* place the cursor so that the next value fs prints will be at the cursor location.
+ this may involve changing the cursor location and/or writing whitespaces to the stream. */
+bool CharDrawer::prepareCursor() {
+  // * start reading from the top of the file, at row=0 col=0.
+  // from there, count how far down we must go. *
+  int nLocation = 0;
+  //!! we have to pre-compute this, or else it will alter the location of our writer in fs. !!
+  int nLengthCursorRow = lengthRow(nCursorRow);
+  for(int i = 0; i < fs.seekg(0, fs.beg); ++i)
+    nLocation += lengthRow(i) + 1; //+ 1 for '\n'.
+  //the order of these 3 steps obviously matters!
+  // * put cursor within extreme bounds *
+  nCursorRow = maxsimp(nCursorRow, 0);
+  nCursorCol = maxsimp(nCursorCol, 0);
+  //nCursorRow = minsimp(nCursorRow, nRowMaxAllowed);
+  //nCursorCol = minsimp(nCursorCol, nColMaxAllowed);
+
+  // * make sure the row exists and position our cursor on it *
+  if (nCursorRow > nRowEnd()) {
+    fs.seekg(0, is.end); //append to end of file
+    nLocation = fs.tellg(0, is.end); //although in this case, it doesn't really matter...
+  } else
+    fs.seekg((nLocation - 1), is.begin); //check for off by 1 error.
+  while (nCursorRow > nRowEnd()) {
+    fs << "\n";
+    nLocation++;
+  }
+  // * make sure the column exists and get in position *
+  // we are already on the row, so let's get to the correct column, or the last column if needbe.
+  int nLocation += minsimp(lengthRow(nCursorRow), nCursorCol - 1);
+  fs.seekg(nLocation, fs.beg);
+  while (nCursorCol > nCursorRow ) //note this is >.
+    fs << " ";
+  // * the cursor is now in position. *
 }
 void CharDrawer::navigate() {
   cout << "Options:\n(d) Return to drawing mode\n(s) Save current file\n(l) Load file\n(c) Create file\n" <<
@@ -80,7 +175,7 @@ void CharDrawer::navigate() {
        << "to save before loading/quitting, either of which will close the currently open file\nwithout saving." << endl;
   char c;
   cin >> c;
-  tolower(c);
+  c = tolower(c);
   switch(c) {
     case 'd': //draw
       draw();
@@ -112,7 +207,7 @@ void CharDrawer::create() {
   cout << strFilename;
    //check if I need 'ios::trunc' here in order to actually create a file. If not, 
    //I am likely to replace the body of this function with a call to load().
-  fs.open(strFilename + EXTENSION, ios::in | ios::out);
+  fs.open(strFilename + EXTENSION, ios::in | ios::out);// | ios::trunc);
   if (!fs.is_open()) {
     strFilename = "";
     cout << "Sorry, " << strFilename << this->EXTENSION << " could not be created." << endl;
@@ -141,7 +236,7 @@ void CharDrawer::save() {
   //seriously, that is basically true, by the very fact I'm using fstream, it seems...
 }
 void CharDrawer::quit() {
-  closefile(); // clearly, this is all we really want to do here.
+  closefile();
   exit(0);
   //c.f. exit(EXIT_FAILURE);
 }
--- CharDrawer.h
+++ CharDrawer.h
@@ -1,31 +1,14 @@
 #pragma once
+//warning: this class assumes Windows operating system.
 #include <string> //do I want to use string? maybe I will need fancy symbols from elsewhere...
 #include <iostream>
 #include <fstream>
 #include <ctype.h> //tolower()
+#include <stdlib.h> //but specifically the Windows version, so as to use system("cls");
+#include <conio.h>
 
-/*
-https://cplusplus.com/doc/tutorial/files/
-https://www.codesdope.com/cpp-file-io/
-https://stackoverflow.com/questions/48085781/what-is-the-difference-among-iosapp-out-and-trunc-in-c#:~:text=ios%3A%3Aout%20is%20the,the%20end%20of%20the%20file.
+#include "UtilA.h" //put the actual path here.
 
-Now, beginning to learn about how to change our cursor position, as it were:...
-    https://stackoverflow.com/questions/31538554/fstream-how-to-seekg-to-position-x-from-end
-        (not really for me, but useful for finding what stuff we deal with here)
-
-https://stackoverflow.com/questions/5570361/file-vs-fstream
-    FILE (C) vs fstream (C++)
-    and, importantly, read the comments about memory management and remembering to close files.
-    or rather, to be wary of 'fflush' in the destructor.
-
-flags needed to io a file versus to create a file 
-    http://eel.is/c++draft/filebuf#tab:filebuf.open.modes
-    ...
-    do I need to use 'trunc' to create? I definitely need to avoid using it to load....
-*/
-
-
-//do I really want to write this as a class? honestly, probably.... .... ?
 
 //what does one save, and what does one do to update the file?
 //in an actual wordprocessor, one should NOT go saving the file after every change.
@@ -33,32 +16,46 @@ flags needed to io a file versus to create a file
 class CharDrawer {
 public:
     enum mode { draw, navigate/*, saveload*/ };
-    char c; //I don't actually have a reason to protect this one...
-    //these just return the index (starting at 0) of the last col and last row that currently exists.
-    int nLastCol() const;
-    int nLastRow() const;
-    void print(bool bDrawingMode = false) const; //prints. we call this after every change.
 
+    void print(bool bDrawingMode = false); //prints. we call this after every change.
     void draw();
     void navigate();
     void load();
     void save();
     void create(); //create new file.
     void closefile();
-    void quit(); // questionable if this really belongs here.
+    void quit(); //questionable if this really belongs here.
 private:
+    // (nCursorRow, nCursorCol) will be the location of the the character drawn by writeValAtCursor,
+    // or in general the character drawn by prepareCursor(); fs << c;
+    // note that for this to work, prepareCursor() sets fs.fset to nCursorRow, but the column right
+    // before nCursorCol
     int nCursorRow;
     int nCursorCol;
-    //const int nColMaxAllowed = 256; //not sure if I will include this... but maybe sometime...
+    //const int nColMaxAllowed = 256; //not sure if I will include this... but maybe sometime... could be lower val, like 80, too...
     //const int nRowMaxAllowed = 256;
     std::string strFilename;
     const char* EXTENSION = ".txtpic";
     std::fstream fs;
+
+
+/*  if the cursor is not at a valid position, we use these routines until it is a valid position,
+    meaning that nCursorRow, nCursorCol is either a location in the picture as stands, or is one column
+    after a location in the picture as stands.
+
+    if the cursor is completely out of bounds we move the cursor itself until it is within the extreme bounds.
+    we add newlines and spaces until the position does exist.
+    
+    additionally, this function makes it so that our filestream is positioned so that the next write operation
+    will write to the valid cursor location.
+    
+    return true if the cursor is unmoved, false if fixing the cursor requires modifying nCursorRow and nCursorCol */
+    bool prepareCursor();
+    void writeValAtCursor(char c);
+
+    //caution! these 3 functions ALTER the position of our filereader!
+    //I could do something to prevent them from doing so, but...
+    int nRowEnd(); //the number of rows, hence, we have rows of indices [0, nRowEnd();).
+    int lengthRow(int nRow);
+    int lengthLongestRow(); //currently not needed.
 }
-
-
-//how does this work?
-//we have an image that we print.
-
-
-
